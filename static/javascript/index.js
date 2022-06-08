a2596ae059ba07f7be0d5dede99aa99ffc0a73e8
Shahnawaz Khan <mailtoshahnawazk@gmail.com>
2022-06-08 20:07:45 +0530

    Completed Code Textarea

---
+++ static/javascript/index.js
@@ -0,0 +1,26 @@
+window.onload=function()
+{
+    document.getElementById('UserFile').addEventListener('change',putContents);
+    document.getElementById('EditorialFile').addEventListener('change',putContents);
+}
+function putContents(event)
+{ 
+    var fileRead= new FileReader();
+    if (this.files && this.files[0])
+    {
+        fileRead.readAsText(this.files[0]);
+        fileRead.onload= function()
+        {
+            if (event.target.id=='UserFile')
+            {
+                document.getElementById('UserCode').innerHTML=fileRead.result;
+            }
+            if (event.target.id=='EditorialFile')
+            {
+                document.getElementById('EditorialCode').innerHTML=fileRead.result;
+            }
+        };
+        
+    }
+     
+}
