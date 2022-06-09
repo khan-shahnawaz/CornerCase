359613dcac187eba00b1cca6cea988d417ff8416
Shahnawaz Khan <mailtoshahnawazk@gmail.com>
2022-06-09 22:10:54 +0530

    Updated models

--- Tester/models.py
+++ Tester/models.py
@@ -1,3 +1,19 @@
+from asyncio.windows_events import NULL
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+class ProgrammingLanguage(models.Model):
+    name=models.CharField(max_length=255)
+    id= models.AutoField(unique=True, primary_key=True, null=False)
+    compileCommand=models.TextField(default=NULL)
+    executeCommand=models.TextField(null=False)
+class Executable(models.Model):
+    status=models.CharField(max_length=255)
+    userCode=models.TextField()
+    userLanguage=models.IntegerField()
+    generatorCode=models.TextField()
+    generatorLanguge=models.IntegerField()
+    editorialCode=models.TextField()
+    editorialLanguage=models.IntegerField()
+    queueNo=models.AutoField(primary_key=True)
--- Tester/views.py
+++ Tester/views.py
@@ -1,6 +1,17 @@
-import imp
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 def index(request):
-    return  render(request,'index.html')
+    languages= ProgrammingLanguage.objects.all()
+    if request.method=='POST':
+        newExecutable=Executable()
+        newExecutable.userCode=request.POST['UserCode']
+        newExecutable.userLanguage=request.POST['UserLanguage']
+        newExecutable.editorialCode=request.POST['EditorialCode']
+        newExecutable.editorialLanguage=request.POST['EditorialLanguage']
+        newExecutable.generatorCode=request.POST['GeneratorCode']
+        newExecutable.generatorLanguge=request.POST['GeneratorLanguage']
+        newExecutable.status="In queue"
+        newExecutable.save()
+    return  render(request,'index.html',{'ProgrammingLanguages':languages})
