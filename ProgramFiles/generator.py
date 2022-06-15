e798108d73a09d4ba757a6586bce3a23b85bef0f
skhan-org <mailtoshahnawazk@gmail.com>
2022-06-15 20:33:32 +0530

    Updated gitignore

--- ProgramFiles/generator.py
+++
@@ -1,8 +0,0 @@
-import random
-tests=random.randint(1,100)
-print(tests)
-for test in range(tests):
-    n=random.randint(1,10)
-    l=[random.randint(1,20) for i in range(n)]
-    print(n)
-    print(*l)
