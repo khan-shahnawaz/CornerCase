e423181ab7e9d3fccf549df6d509ee3685e51f32
skhan-org <mailtoshahnawazk@gmail.com>
2022-06-24 13:36:25 +0530

    Updated Settings

--- CornerCase/settings.py
+++ CornerCase/settings.py
@@ -26,7 +26,7 @@ DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: update this when you have the production host
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = [".herokuapp.com"]
 
 
 # Application definition
