976e4013f6c2009f5faaed67d43fa152fd164a8c
skhan-org <mailtoshahnawazk@gmail.com>
2022-06-24 22:13:32 +0530

    Changed wsgi.py

--- CornerCase/wsgi.py
+++ CornerCase/wsgi.py
@@ -13,5 +13,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CornerCase.settings')
+from dj_static import Cling
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
+#application = get_wsgi_application()
