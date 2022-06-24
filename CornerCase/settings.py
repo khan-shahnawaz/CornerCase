f9139cf7b22a8219c4915e867a1986c2cf85233e
skhan-org <mailtoshahnawazk@gmail.com>
2022-06-24 14:18:41 +0530

    Updated Settings

--- CornerCase/settings.py
+++ CornerCase/settings.py
@@ -27,9 +27,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (
